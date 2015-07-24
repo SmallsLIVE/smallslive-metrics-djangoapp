ae187537245f344e938a5144f4fe142da89f84a7
Filip Jukic <filip@jukic.me>
2015-07-24 14:01:23 +0200

    reorganization of metrics methods

--- metrics/models.py
+++ metrics/models.py
@@ -8,61 +8,37 @@ from django.utils import timezone
 
 
 class MetricsQuerySet(models.QuerySet):
-    def total_plays(self):
-        return self.aggregate(play_count=Sum('play_count'))
-
-    def total_seconds(self):
-        return self.aggregate(seconds_played=Sum('seconds_played'))
+    def total_counts(self):
+        return self.aggregate(seconds_played=Sum('seconds_played'), play_count=Sum('play_count'))
 
 
 class MetricsManager(models.Manager):
     def get_queryset(self):
         return MetricsQuerySet(self.model, using=self._db)
 
-    def total_seconds_for_recording(self, recording_id):
-        return self.get_queryset().filter(recording_id=recording_id).total_seconds()
+    def counts_for_recording(self, recording_id):
+        return self.get_queryset().filter(recording_id=recording_id).total_counts()
 
-    def total_plays_for_recording(self, recording_id):
-        return self.get_queryset().filter(recording_id=recording_id).total_plays()
+    def counts_for_artist(self, artist_recording_ids):
+        return self.get_queryset().filter(recording_id__in=artist_recording_ids).total_counts()
 
-    def total_seconds_for_artist(self, artist_recording_ids):
-        return self.get_queryset().filter(recording_id__in=artist_recording_ids).total_seconds()
+    def total_counts(self):
+        return self.get_queryset().total_counts()
 
-    def total_plays_for_artist(self, artist_recording_ids):
-        return self.get_queryset().filter(recording_id__in=artist_recording_ids).total_plays()
+    def counts_for_month(self, month, year):
+        return self.get_queryset().filter(date__month=month, date__year=year).total_counts()
 
-    def total_plays(self):
-        return self.get_queryset().total_plays()
-
-    def total_seconds(self):
-        return self.get_queryset().total_seconds()
-
-    def total_plays_for_month(self, month, year):
-        return self.get_queryset().filter(date__month=month, date__year=year).total_plays()
-
-    def total_seconds_for_month(self, month, year):
-        return self.get_queryset().filter(date__month=month, date__year=year).total_seconds()
-
-    def date_plays_for_recording(self, recording_id, month, year):
+    def date_counts_for_recording(self, recording_id, month, year):
         values = self.get_queryset().filter(
-            recording_id=recording_id, date__month=month, date__year=year).values_list('date', 'play_count').order_by('date')
+            recording_id=recording_id, date__month=month, date__year=year).values_list(
+            'date', 'play_count', 'seconds_played').order_by('date')
         days_in_month = calendar.monthrange(year, month)[1]
         days = range(1, days_in_month+1)
-        day_values = {date.day: count for date, count in values}
-        values_list = [day_values.get(day_number, 0) for day_number in days]
-        return values_list
-
-    def date_seconds_for_recording(self, recording_id, month, year):
-        values = self.get_queryset().filter(
-            recording_id=recording_id, date__month=month, date__year=year).values_list('date', 'seconds_played').order_by('date')
-        days_in_month = calendar.monthrange(year, month)[1]
-        days = range(1, days_in_month+1)
-        day_values = {date.day: count for date, count in values}
-        values_list = [day_values.get(day_number, 0) for day_number in days]
-        return values_list
-
-    def full_stats_for_recording(self, recording_id):
-        return self.get_queryset().filter(recording_id=recording_id).total_plays()
+        day_counts = {date.day: count for date, count, seconds in values}
+        day_seconds = {date.day: count for date, count, seconds in values}
+        counts_list = [day_counts.get(day_number, 0) for day_number in days]
+        seconds_list = [day_seconds.get(day_number, 0) for day_number in days]
+        return (counts_list, seconds_list)
 
     def create_random(self):
         today = timezone.now().date()

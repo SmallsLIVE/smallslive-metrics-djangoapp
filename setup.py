0978e460e2475c1d1ec82a7c1777b0b893ec2d39
Filip Jukić <filip@jukic.me>
2015-08-06 20:29:30 +0200

    export metrics_users package

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='smallslive-metrics-app',
-    version='0.1.16',
-    packages=['metrics'],
+    version='0.1.17',
+    packages=['metrics', 'metrics_users'],
     include_package_data=True,
     license='BSD License',  # example license
     description='',

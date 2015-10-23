b3cf59fa35cb4ffc864e5456d1560c93c3c3c923
Filip Jukić <filip@jukic.me>
2015-10-23 15:52:36 +0200

    version bump to 0.1.34

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='smallslive-metrics-app',
-    version='0.1.33',
+    version='0.1.34',
     packages=['metrics', 'metrics_users'],
     include_package_data=True,
     license='BSD License',  # example license

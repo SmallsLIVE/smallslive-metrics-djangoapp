5a44004d141569dad4af5568be4063bf8081c7ea
Martin Prunell <martin.prunell@gmail.com>
2019-03-08 10:35:48 -0500

    Bump version to 0.1.43

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='smallslive-metrics-app',
-    version='0.1.42',
+    version='0.1.43',
     packages=['metrics', 'metrics.migrations', 'metrics_users'],
     include_package_data=True,
     license='BSD License',  # example license
